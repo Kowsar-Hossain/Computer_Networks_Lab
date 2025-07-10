b970e9b67fb5d767933df09f3c61972e3c5ca73a
Md Kowsar Hossain <149518355+Kowsar-Hossain@users.noreply.github.com>
2025-07-10 08:34:32 +0600

    Client_server_assignment

--- Client_server_assignment.java
+++ Client_server_assignment.java
@@ -36,39 +36,43 @@ public class client{
 
 
 
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
-public class client{
-    public static void main(String s1[]){
+public class Server{
+    public static void main(String fucks[]){
         try{
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Input Server IP: ");
-            String IP = scanner.nextLine();
-            Socket socket = new Socket(IP,7000);
-
-            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            // Creating server-socket on localhost port 7000
+            ServerSocket serverSocket = new ServerSocket(7000);
+            System.out.println("Server is running on the port 7000 ...");
+            
+            Socket socket = serverSocket.accept();
+            DataInputStream doi = new DataInputStream(socket.getInputStream());
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-
+            Scanner scanner = new Scanner(System.in);
             while(true){
-                System.out.print("You: ");
-                String message = scanner.nextLine();
-                dos.writeUTF(message);
-                String serverResponse = dis.readUTF();
-                System.out.println("Server: " + serverResponse);
-                if(serverResponse.equals("Bye")){
-                    break;
+                String message = doi.readUTF();
+                System.out.println("Client: " + message);
+                
+                if (message.equals("Exit")) {
+                    dos.writeUTF("Bye");
+                    break; // Exit the inner loop after "Exit" command
                 }
+                System.out.print("Server: ");
+                message = scanner.nextLine();
+                dos.writeUTF(message);
             }
             scanner.close();
-            dis.close();
+            doi.close();
             dos.close();
             socket.close();
         }catch(Exception e){
             System.out.println("Error: " + e.getMessage());
-        }
+        }    
     }
 }
